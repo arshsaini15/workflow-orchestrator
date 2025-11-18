c1e4230a2f888876bb7c0b917201b9b63044052d
arshsaini15 <arshdeepp015@gmail.com>
2025-11-19 03:04:01 +0530

    Initial commit

---
+++ src/test/java/org/arsh/workflow_orchestrator/WorkflowOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arsh.workflow_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
