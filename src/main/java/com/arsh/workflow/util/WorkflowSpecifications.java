4bd767e439e79c03a3ae65f67fe7cecd1e72ed44
arshsaini15 <arshdeepp015@gmail.com>
2025-12-04 12:49:29 +0530

    JPA Specification Executor Implemented : Dynamic SQL queries at runtime.

---
+++ src/main/java/com/arsh/workflow/util/WorkflowSpecifications.java
@@ -0,0 +1,39 @@
+package com.arsh.workflow.util;
+
+import com.arsh.workflow.enums.WorkflowStatus;
+import com.arsh.workflow.model.Workflow;
+import org.springframework.data.jpa.domain.Specification;
+
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkflowSpecifications {
+
+    public static Specification<Workflow> filter(
+            WorkflowStatus status,
+            String createdBy,
+            String search
+    ) {
+        return (root, query, cb) -> {
+
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (status != null) {
+                predicates.add(cb.equal(root.get("status"), status));
+            }
+
+            if (createdBy != null && !createdBy.isBlank()) {
+                predicates.add(cb.equal(root.get("createdBy"), createdBy));
+            }
+
+            if (search != null && !search.isBlank()) {
+                predicates.add(
+                        cb.like(cb.lower(root.get("name")), "%" + search.toLowerCase() + "%")
+                );
+            }
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
